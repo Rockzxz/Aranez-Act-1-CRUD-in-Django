193f9e8e5af51bedd13c2c6d0380942f10be3e0b
Rocky <rockyaranez@gmail.com>
2026-02-07 11:14:07 +0800

    Final Submission

--- ProjectTracker/__init__.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-pymysql.install_as_MySQLdb() #pymysql gamit kay way C++ Compiler.
-
-#trick kay di mugana ako mysql
-import MySQLdb
-MySQLdb.version_info = (2, 2, 7, 'final', 0)
-MySQLdb.__version__ = '2.2.7'
--- ProjectTracker/settings.py
+++ ProjectTracker/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'ProjectTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'project_tracker_db',  # Must match phpMyAdmin
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
